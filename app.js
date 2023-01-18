24c128ae41444022cc58f79c22b0ddc6cdf9655d
Miguel <itsmig@proton.me>
2023-01-17 18:34:07 -0800

    created view controller function that will display html content.

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("live");
+// make an array of questions, 1 question per item (20 items).
+// create a function that iterates through the questions displaying each following question as the user answers and moves to the next question.
+
+const questionOne = `<h2 class="sub-header">
+          (new title) an online browser game inspired by "The Price Is Right"
+        </h2>
+        <p class="para-text">
+          (title) is a online browser game where you have to guess the price of
+          each shown item. Getting the answer correct will earn you 100 points.
+          You have to reach a total score of 1,000 points to win, and you only
+          have 20 items to guess from. You have to earn the 1,000 points before
+          or on the 20th-item question. or you lose the game, good luck!
+        </p> 
+        <button class="btn">Begin Game</button>`;
+
+// view controller function
+function switchView(e) {
+  document.querySelector("#main-content").innerHTML = e;
+}
+
+switchView(questionOne);
