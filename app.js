a7d9f471b095cf1610f7bbd8c9afdb74205b23f5
Miguel <itsmig@proton.me>
2023-01-17 21:07:49 -0800

    added css, colors, background colors, using flexbox to position html content.

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 // make an array of questions, 1 question per item (20 items).
 // create a function that iterates through the questions displaying each following question as the user answers and moves to the next question.
+// add a text box for score tracking in our questions array
 
-const questionOne = `<h2 class="sub-header">
-          (new title) an online browser game inspired by "The Price Is Right"
+const mainContent = `<h2 class="sub-header">
+          Name Your Price is an online browser game inspired by "The Price Is Right"
         </h2>
         <p class="para-text">
-          (title) is a online browser game where you have to guess the price of
+          <span class="span-one">Name Your Price</span> is an online browser game where you have to guess the price of
           each shown item. Getting the answer correct will earn you 100 points.
           You have to reach a total score of 1,000 points to win, and you only
           have 20 items to guess from. You have to earn the 1,000 points before
           or on the 20th-item question. or you lose the game, good luck!
         </p> 
-        <button class="btn">Begin Game</button>`;
+        <button class="btn">Begin Game</button> `;
 
 // view controller function
 function switchView(e) {
   document.querySelector("#main-content").innerHTML = e;
 }
 
-switchView(questionOne);
+switchView(mainContent);
