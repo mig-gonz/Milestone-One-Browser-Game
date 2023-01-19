8cc0d457aba5faedda72e626f355638fde5eeefc
Miguel <itsmig@proton.me>
2023-01-18 17:27:53 -0800

    updated title. added multiple background images. added css

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // make an array of questions, 1 question per item (20 items).
 // create a function that iterates through the questions displaying each following question as the user answers and moves to the next question.
 // add a text box for score tracking in our questions array
+// add four buttons for price answer options
 
 const mainContent = `<h2 class="sub-header">
-          Name Your Price is an online browser game inspired by "The Price Is Right"
+          Bid to Win, inspired by "The Price Is Right"
         </h2>
         <p class="para-text">
-          <span class="span-one">Name Your Price</span> is an online browser game where you have to guess the price of
+          <span class="span-one">Bid to Win</span>: an online browser game where you have to guess the price of
           each shown item. Getting the answer correct will earn you 100 points.
           You have to reach a total score of 1,000 points to win, and you only
           have 20 items to guess from. You have to earn the 1,000 points before
